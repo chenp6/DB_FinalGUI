1432b761e0c983a5ecde18310e8e3c445fcc1c95
chenp6 <52535982+chenp6@users.noreply.github.com>
2021-09-15 04:21:43 +0800

    Add files via upload

---
+++ src/Basket.java
@@ -0,0 +1,12 @@
+package Query5.Basket;
+
+public class Basket {
+	public String UPC;
+	public int amount;
+
+	
+	public Basket(String UPC,int amount){
+	    this.UPC = UPC;
+	    this.amount = amount;
+	}
+}
